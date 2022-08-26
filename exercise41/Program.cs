219bfa92a7883cd6f1dd4b187a05f4fbab2eb33c
Anton <anton_artemjev06@mail.ru>
2022-08-27 00:06:56 +0700

    ДЗ 6

---
+++ exercise41/Program.cs
@@ -0,0 +1,46 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+
+int M = 5;
+
+int b = 0;
+
+int [] numbers = new int[M];
+
+
+for (int i = 0; i < M; i++)
+ {
+    System.Console.WriteLine("Введите число");
+    numbers [i] = int.Parse(Console.ReadLine()!);
+    
+ }
+
+Console.Write("[");
+for (int i = 0; i < numbers.Length; i++)
+ {
+    
+    Console.Write(" " + numbers [i] + " ");
+ }
+
+
+Console.Write("]");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    if (numbers [i] > 0)
+    {
+        b+=1;
+    }
+    
+ }
+
+
+
+
+Console.Write(" -> " + b);
+
+
+
---
+++ exercise43/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.WriteLine("Введите b1");
+int b1 = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите k1");
+int k1 = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите b2");
+int b2 = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите k2");
+int k2 = int.Parse(Console.ReadLine()!);
+
+
+double a = k1 - k2;
+double b = b2 - b1;
+
+double x = b / a;
+
+double y = k1 * x + b1;
+
+System.Console.WriteLine(" (" + x + "; " + y + ")");
+
+
